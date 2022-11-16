71fb0e8624ad1a6dfe5ae48f085b0dd764cc0b1a
Ashley <admin@ashleybailey.me>
2022-11-16 20:11:56 +0000

    test

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -122,6 +122,34 @@ public extension View {
     ///
     /// - Parameter bottomSheetPosition: A binding that holds the current position/state of the BottomSheet.
     /// For more information about the possible positions see `BottomSheetPosition`.
+    /// - Parameter switchablePositions: An array that contains the positions/states of the BottomSheet.
+    /// Only the positions/states contained in the array can be switched into
+    /// (via tapping the drag indicator or swiping the BottomSheet).
+    /// - Parameter headerContent: A view that is used as header content for the BottomSheet.
+    /// You can use a String that is displayed as title instead.
+    /// - Parameter mainContent: A view that is used as main content for the BottomSheet.
+    func bottomSheet<AContent: View, HContent: View, MContent: View>(
+        bottomSheetPosition: Binding<BottomSheetPosition>,
+        switchablePositions: [BottomSheetPosition],
+        @ViewBuilder headerContent: () -> HContent? = {
+            return nil
+        },
+        @ViewBuilder mainContent: () -> MContent
+    ) -> BottomSheet<AContent, HContent, MContent, Self> {
+        BottomSheet(
+            bottomSheetPosition: bottomSheetPosition,
+            switchablePositions: switchablePositions,
+            aboveContent: nil,
+            headerContent: headerContent(),
+            mainContent: mainContent(),
+            view: self
+        )
+    }
+    
+    /// Adds a BottomSheet to the view.
+    ///
+    /// - Parameter bottomSheetPosition: A binding that holds the current position/state of the BottomSheet.
+    /// For more information about the possible positions see `BottomSheetPosition`.
     /// - Parameter switchablePositions: An array that contains the positions/states for the BottomSheet.
     /// Only the positions/states contained in the array can be switched into
     /// (via tapping the drag indicator or swiping the BottomSheet).
@@ -133,16 +161,13 @@ public extension View {
     func bottomSheet<AContent: View, MContent: View>(
         bottomSheetPosition: Binding<BottomSheetPosition>,
         switchablePositions: [BottomSheetPosition],
-        @ViewBuilder aboveContent: () -> AContent? = {
-            return nil
-        },
         title: String? = nil,
         @ViewBuilder content: () -> MContent
     ) -> BottomSheet<AContent, TitleContent, MContent, Self> {
         BottomSheet(
             bottomSheetPosition: bottomSheetPosition,
             switchablePositions: switchablePositions,
-            aboveContent: aboveContent(),
+            aboveContent: nil,
             title: title,
             content: content(),
             view: self
